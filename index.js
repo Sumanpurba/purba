978cc0e441e6fb5a2de5f53d79bf49caae0ea9a3
Sumanpurba <47668599+Sumanpurba@users.noreply.github.com>
2019-02-15 12:52:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-import express from 'express';
-import bodyParser  from "body-parser";
-import Game  from "./Game";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// Create a new express application instance
-const app = express();
+ReactDOM.render(<App />, document.getElementById('root'));
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("www"));
-
-app.get("/users/:uname", (req, res) => {
-    res.end("Hello " + req.params.uname);
-});
-
-let oGames = {};
-app.post("/sms", (req, res) =>{
-    let sFrom = req.body.From || req.body.from;
-    if(!oGames.hasOwnProperty(sFrom)){
-        oGames[sFrom] = new Game();
-    }
-    let sMessage = req.body.Body|| req.body.body;
-    let aReply = oGames[sFrom].makeAMove(sMessage);
-    res.setHeader('content-type', 'text/xml');
-    let sResponse = "<Response>";
-    for(let n = 0; n < aReply.length; n++){
-        sResponse += "<Message>";
-        sResponse += aReply[n];
-        sResponse += "</Message>";
-    }
-    res.end(sResponse + "</Response>");
-
-});
-
-var port = process.env.PORT || parseInt(process.argv.pop()) || 3002;
-
-app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
